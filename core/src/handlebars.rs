29a202231d97e5e61a3c834060435fd541b9bf50
lgmys <lukasz.gmys@elastic.co>
2022-11-21 16:57:47 +0100

    use macro_rules to register handlebars helpers

--- core/src/handlebars.rs
+++ core/src/handlebars.rs
@@ -1,12 +1,6 @@
 use handlebars::{handlebars_helper, Handlebars};
 
-use fake::{
-    faker::{
-        internet::en::{DomainSuffix, IPv4, IPv6, Username},
-        lorem::en::Word,
-    },
-    Fake,
-};
+use fake::Fake;
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
 
 use chrono::{Duration, Utc};
@@ -22,6 +16,8 @@ pub fn create<'a>() -> Handlebars<'a> {
         dt.format(FORMAT_ISO).to_string()
     });
 
+    let mut handlebars = Handlebars::new();
+
     handlebars_helper!(random_string: |*_args|  thread_rng()
             .sample_iter(&Alphanumeric)
             .take(16)
@@ -30,26 +26,26 @@ pub fn create<'a>() -> Handlebars<'a> {
 
     );
 
-    // see https://crates.io/crates/fake
-
-    handlebars_helper!(random_username: |*_args| Username().fake::<String>());
-    handlebars_helper!(random_domain: |*_args| DomainSuffix().fake::<String>());
-    handlebars_helper!(random_word: |*_args| Word().fake::<String>());
-    handlebars_helper!(random_ipv4: |*_args| IPv4().fake::<String>());
-    handlebars_helper!(random_ipv6: |*_args| IPv6().fake::<String>());
-
-    let mut handlebars = Handlebars::new();
+    macro_rules! helpers {
+        (    $hb:ident, $($i:ident : $p:path), *) => {
+                $(
+                    handlebars_helper!($i: |*_args| $p().fake::<String>());
+                    handlebars.register_helper(stringify!($i), Box::new($i));
+                )*
+        };
+    }
 
     handlebars.register_helper("string", Box::new(random_string));
     handlebars.register_helper("date_iso", Box::new(random_iso_date));
 
-    handlebars.register_helper("word", Box::new(random_word));
-
-    handlebars.register_helper("domain", Box::new(random_domain));
-    handlebars.register_helper("ipv4", Box::new(random_ipv4));
-    handlebars.register_helper("ipv6", Box::new(random_ipv6));
-
-    handlebars.register_helper("username", Box::new(random_username));
+    helpers!(
+        handlebars,
+        username: fake::faker::internet::en::Username,
+        domain: fake::faker::internet::en::DomainSuffix,
+        ipv4: fake::faker::internet::en::IPv4,
+        ipv6: fake::faker::internet::en::IPv6,
+        word: fake::faker::lorem::en::Word
+    );
 
     return handlebars;
 }
